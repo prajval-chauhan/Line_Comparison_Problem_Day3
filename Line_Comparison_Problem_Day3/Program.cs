57c0ab5e256b9afc98f8b2a141abe528e9e2d50d
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-04 07:18:25 +0530

    UC1: to calculate the distance between two given points

--- Line_Comparison_Problem_Day3/Program.cs
+++ Line_Comparison_Problem_Day3/Program.cs
@@ -8,6 +8,14 @@ namespace Line_Comparison_Problem_Day3
         {
             Console.WriteLine("Welcome to the Line Comparison Computation Program");
 
+            Console.WriteLine("Enter the coordinates of the first point: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the coordinates of the second point: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double distance = (Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+            Console.WriteLine("The distance between the given points is: " + distance);
         }
     }
 }
